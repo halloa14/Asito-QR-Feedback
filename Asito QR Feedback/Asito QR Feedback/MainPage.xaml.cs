7b9d5227c04339092703e24e1e45d04ec13950ed
Sander Bloemen <s.p.r.bloemen@student.utwente.nl>
2018-05-27 16:48:12 +0200

    Eerste commit

---
+++ Asito QR Feedback/Asito QR Feedback/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Asito_QR_Feedback
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
